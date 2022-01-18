24632a5c93b08a1cffbf113548f836259b3beac5
Kieran Ricardo <kieranricardo@hotmail.com>
2022-01-18 11:48:55 +1100

    edit

--- examples/demo.py
+++ examples/demo.py
@@ -12,10 +12,6 @@ print(events[0].head()[["latitude", "pcentre", "windspeed", "rmax", "poci"]])
 for event in events:
     member = event.iloc[0]['member']
     disturbance = event.iloc[0]['disturbance']
-    event['Vmax1'] = 0.54 * event['windspeed'] / 0.88
-    # Update the translation speed
-    event = translation_speed(event)
-    event['Vsrm'] = event.apply(lambda x: storm_relative(x['Vmax1'], x['translation_speed']), axis=1)
-    event['x'] = event.apply(lambda x: vortex_param(x['Vmax1'], x['latitude']), axis=1)
+
     outfilename = os.path.join(outputPath, f"{disturbance}.{member}.csv")
     event.to_csv(outfilename, float_format="%.2f")
